d77779358fc6bfd327647d270006c8411c8591a3
I-am-Som <somanathmaharanaofficial@gmail.com>
2024-12-29 21:15:40 +0530

    tryinh to learn ArrayList

---
+++ Collection Interface/1.List/1.ArraytList/T1_ArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;;
+
+public class T1_ArrayList {
+    public static void main(String[] args) {
+        
+        ArrayList al = new ArrayList();
+        al.add(456);
+        al.add("somu");
+        al.add(98.75f);
+        al.add(true);
+        
+        System.out.println(al);
+
+    }
+}
