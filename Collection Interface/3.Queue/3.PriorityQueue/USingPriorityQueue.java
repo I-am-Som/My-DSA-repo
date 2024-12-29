0f89b6c27d6c08d6570e0e0ad1fbca258acf97c6
I-am-Som <somanathmaharanaofficial@gmail.com>
2024-12-29 22:45:19 +0530

    PriorityQueue?

---
+++ Collection Interface/3.Queue/3.PriorityQueue/USingPriorityQueue.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class USingPriorityQueue {
+    
+    public static void main(String[] args) {
+        
+        PriorityQueue pq = new PriorityQueue();
+        pq.add(54);
+        pq.add(5);
+        pq.add(12);
+        pq.add(46);
+        pq.add(31);
+
+        System.out.println(pq);
+    }
+}
