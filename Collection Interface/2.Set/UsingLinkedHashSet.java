133b2cae8aa8f64810537553e7c8669827ee4864
I-am-Som <somanathmaharanaofficial@gmail.com>
2024-12-30 17:13:10 +0530

    same as hastSet but maintains order

---
+++ Collection Interface/2.Set/UsingLinkedHashSet.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UsingLinkedHashSet {
+    
+    public static void main(String[] args) {
+        
+        LinkedHashSet lhs = new LinkedHashSet();
+
+        lhs.add(50);
+        lhs.add(96);
+        lhs.add(15);
+        lhs.add(43);
+        lhs.add(72);
+
+        System.out.println(lhs);
+    }
+}
