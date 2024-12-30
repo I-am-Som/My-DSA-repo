3ad17d0765e99467079e0ac867c29e5481a34c89
I-am-Som <somanathmaharanaofficial@gmail.com>
2024-12-30 17:03:57 +0530

    easy peasy

---
+++ Collection Interface/2.Set/UsingTreeSet.java
@@ -0,0 +1,18 @@
+import java.util.*;;
+
+public class UsingTreeSet {
+    
+    public static void main(String[] args) {
+        
+        TreeSet ts = new TreeSet();
+        ts.add(45);
+        ts.add(98);
+        ts.add(4);
+        ts.add(56);
+        ts.add(19);
+        ts.add(77);
+
+        System.out.println(ts);
+
+    }
+}
