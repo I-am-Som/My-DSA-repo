15e20c53d906554bbf7c7595a376279efffead7e
I-am-Som <somanathmaharanaofficial@gmail.com>
2024-12-30 17:08:54 +0530

    HashSet for searching because its complexity is O(1)

---
+++ Collection Interface/2.Set/UsingHashSet.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class UsingHashSet {
+    
+    public static void main(String[] args) {
+        
+        HashSet hs = new HashSet();
+
+        hs.add(46);
+        hs.add(13);
+        hs.add(65);
+        hs.add(23);
+        hs.add(78);
+
+        System.out.println(hs);
+
+    }
+}
