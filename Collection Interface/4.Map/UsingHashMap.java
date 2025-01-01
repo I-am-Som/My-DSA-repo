cd854702d0b7585882860f3ba749f9ed3154dad5
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-01-01 22:30:55 +0530

    hashmap need??

---
+++ Collection Interface/4.Map/UsingHashMap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.HashMap;
+
+public class UsingHashMap {
+    public static void main(String[] args) {
+        
+        HashMap hm = new HashMap<>();
+        hm.put(45, "Hello");
+        hm.put(96, "How");
+        hm.put(23, "are");
+        hm.put(93, "you");
+        hm.put(12, "doing?");
+
+        System.out.println(hm);
+
+    }
+    
+}
