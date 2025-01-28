c48bc34315d1cd4ce2536c070769fdbfac4998cc
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-01-28 23:40:37 +0530

    best

---
+++ SolvedLeetcode/TwoSum.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+
+        return res;
+    }
+    
+}
