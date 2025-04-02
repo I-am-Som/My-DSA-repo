bda809f625b7c65da0a9c1fa582a94404232e87e
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-04-02 23:14:08 +0530

    ...

--- SolvedLeetcode/TwoSum.java
+++ SolvedLeetcode/TwoSum.java
@@ -1,10 +1,18 @@
+import java.util.HashMap;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         
         HashMap<Integer, Integer> hm = new HashMap<>();
         
-
+        for (int i : nums) {
+            hm.put(nums[i], i);
+            
+            if(hm.containsKey(target - nums[i])) {
+                int[] res = {hm.get(nums[i]), hm.get(target - nums[i])};
+                return res;
+            }
+        }
         return res;
     }
-    
 }
