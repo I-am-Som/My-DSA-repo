772fa61ca64095b1e9dc502db900e93c13028e5c
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-04-03 22:26:45 +0530

    ...

--- Multi-threading/MyThread.java
+++ Multi-threading/MyThread.java
@@ -9,5 +9,8 @@ public class Main {
     public static void main(String[] args) {
         MyThread th = new MyThread();
         th.start();
+
+        MyThread th2 = new MyThread();
+        th2.start();
     }
 }
